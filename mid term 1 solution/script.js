ad57202f3f2c182978f9df5dfe10c02d601623c0
Ganesh patel <126665724+ganeshpatel1409@users.noreply.github.com>
2026-04-09 11:40:09 +0530

    Add files via upload

---
+++ mid term 1 solution/script.js
@@ -0,0 +1,15 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById("result").innerHTML = "Please enter valid numbers!";
+        return;
+    }
+    let sum = num1 + num2;
+    let difference = num1 - num2;
+    let product = num1 * num2;
+    document.getElementById("result").innerHTML =
+        "Sum: " + sum + "<br>" +
+        "Difference: " + difference + "<br>" +
+        "Product: " + product;
+}
